03ab0d6be99784b8f347f2641015cd055d2278d3
Esteven Valencia <evalencia@unomaha.edu>
2025-03-19 14:37:22 -0500

    name change

--- Tech/migrations/0002_remove_contact_phone_contact_email.py
+++ Tech/migrations/0002_remove_contact_phone_contact_email.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contacts', '0001_initial'),
+        ('Tech', '0001_initial'),
     ]
 
     operations = [
